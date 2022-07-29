46f9f361653afa9b6a0d2610cc432c65a351004c
Afrosweetness2002 <abdullatiffarah21@gmail.com>
2022-07-29 15:19:31 +0100

    I am now continuing on the project after completing the deck of cards.

--- PvC/script.js
+++ PvC/script.js
@@ -74,20 +74,76 @@ const whitebeard = new AnimeCard(
 const goku = new AnimeCard(14, "Goku", 90, 95, 40, "../images/goku.jpg");
 const jiraya = new AnimeCard(15, "Jiraya", 60, 55, 30, "../images/jiraya.jpg");
 const kaneki = new AnimeCard(16, "Kaneki", 40, 35, 30, "../images/kaneki.jpg");
-const light = new AnimeCard(17, "Light Yagami", 10, 05, 99, "../images/light.jpg");
-const shikamaru = new AnimeCard(18, "Shikamaru", 47, 35, 80, "../images/shikamaru.jpg");
-const madara = new AnimeCard(19, "Madara Uchiha", 78, 78, 78, "../images/madara.jpg");
-const orochimaru = new AnimeCard(20, "Orochimaru", 47, 55, 66, "../images/orochimaru.jpg");
-const itachi = new AnimeCard(21, "Itachi Uchiha", 70, 68, 54, "../images/itachi.jpg");
+const light = new AnimeCard(
+  17,
+  "Light Yagami",
+  10,
+  05,
+  99,
+  "../images/light.jpg"
+);
+const shikamaru = new AnimeCard(
+  18,
+  "Shikamaru",
+  47,
+  35,
+  80,
+  "../images/shikamaru.jpg"
+);
+const madara = new AnimeCard(
+  19,
+  "Madara Uchiha",
+  78,
+  78,
+  78,
+  "../images/madara.jpg"
+);
+const orochimaru = new AnimeCard(
+  20,
+  "Orochimaru",
+  47,
+  55,
+  66,
+  "../images/orochimaru.jpg"
+);
+const itachi = new AnimeCard(
+  21,
+  "Itachi Uchiha",
+  70,
+  68,
+  54,
+  "../images/itachi.jpg"
+);
 const kaidou = new AnimeCard(22, "Kaidou", 79, 80, 61, "../images/Kaidou.jpg");
 const gojo = new AnimeCard(23, "Gojo Satoru", 58, 47, 37, "../images/gojo.jpg");
-const natsu = new AnimeCard(24, "Natsu Dragneel", 39, 43, 23, "../images/natsu.jpg");
-const eren = new AnimeCard(25, "Eren Yeager", 47, 25,69, "../images/eren.jpg");
+const natsu = new AnimeCard(
+  24,
+  "Natsu Dragneel",
+  39,
+  43,
+  23,
+  "../images/natsu.jpg"
+);
+const eren = new AnimeCard(25, "Eren Yeager", 47, 25, 69, "../images/eren.jpg");
 const shinra = new AnimeCard(26, "Shinra", 43, 32, 21, "../images/shinra.jpg");
 const gon = new AnimeCard(27, "Gon Freecess", 35, 33, 35, "../images/gon.jpg");
-const tanjiro = new AnimeCard(28, "Tanjiro", 34, 34, 31, "../images/tanjiro.jpg");
+const tanjiro = new AnimeCard(
+  28,
+  "Tanjiro",
+  34,
+  34,
+  31,
+  "../images/tanjiro.jpg"
+);
 const deku = new AnimeCard(29, "Deku", 35, 37, 48, "../images/deku.jpg");
-const allMight = new AnimeCard(30, "All Might", 45, 50, 49, "../images/light.jpg");
+const allMight = new AnimeCard(
+  30,
+  "All Might",
+  45,
+  50,
+  49,
+  "../images/light.jpg"
+);
 
 let deckOfCards = {
   naruto,
